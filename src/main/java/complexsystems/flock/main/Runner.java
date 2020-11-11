30656ad266d9a4cf447082c2893eb5d2f7d2a5fe
michal <michalkaminski82@gmail.com>
2020-11-11 20:56:16 +0100

    fixing velocity matching

--- src/main/java/complexsystems/flock/main/Runner.java
+++ src/main/java/complexsystems/flock/main/Runner.java
@@ -10,15 +10,16 @@ import java.util.List;
 
 public class Runner {
 
-    private static final double BIRD_MAGNET_FORCE = 10;
+    private static final double BIRD_MAGNET_FORCE = 40;
+    private static final double DISTANCE = 0.5;
 
     public static void main(String[] args) {
 
         List<Flock> flocks =new ArrayList<>();
         Screen screen = new Screen(1000, 1000, flocks);
-        Flock f1 = new Flock(2000, 10, 500, -500, 500, -500);
+        Flock f1 = new Flock(1000, 10, 500, -500, 500, -500);
 
-//        f1.addRule(new KeepDistance(40, f1, 8));
+        f1.addRule(new KeepDistance(DISTANCE, f1, 8));
         f1.addRule(new MatchVelocity(BIRD_MAGNET_FORCE));
         flocks.add(f1);
 
